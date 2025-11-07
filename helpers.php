b8c8e8c93a7b2c7d3015b004662e8e64e88d3fc4
arvind kumar <ak2020365@gmail.com>
2025-11-07 09:22:02 +0530

    update

--- helpers.php
+++ helpers.php
@@ -11,3 +11,69 @@ function base_path($path = '')
 {
     return __DIR__ . '/' . $path;
 }
+
+/**
+ * load a view
+ * 
+ * @param string ($name)
+ * @return void
+ * 
+ */
+
+function loadView($name){
+   $viewPath = base_path("views/{$name}.php");
+
+   if(file_exists($viewPath)){
+    require $viewPath;
+   } else {
+    echo "view '{$name} Not Found!'";
+   }
+}
+
+/**
+ * load partials
+ * 
+ * @param string $name
+ * @return void
+ * 
+ */
+
+function loadPartial($name)
+{
+   $partialPath = base_path("views/partials/{$name}.php");
+
+    if(file_exists($partialPath)){
+    require $partialPath;
+   } else {
+    echo "view '{$name} Not Found!'";
+   }
+}
+
+/**
+ * inspect a value(s)
+ *
+ * @param mixed $value
+ * @return value
+ *  
+ */
+
+function inspect ($value){
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+}
+
+
+/**
+ * inspect a value(s) and die
+ *
+ * @param mixed $value
+ * @return value
+ *  
+ */
+
+function inspectAndDie ($value){
+    echo "<pre>";
+    die(var_dump($value));
+    echo "</pre>";
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
 require '../helpers.php';
-require '../views/home.php';
+
+$route = [
+    '/' => 'controllers/home.php',
+    '/listing' => 'controllers/listings/index.php',
+    '/listings/create' => 'controllers/listings/create.php',
+    '404' => 'controllers/error/404.php'
+];
